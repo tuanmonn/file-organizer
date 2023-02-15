0cfe6b430c42ad68208875ff7a0ac2fd11c8dec6
Tuanmon <hungtuan.nguyen@shopee.com>
2023-02-15 16:44:41 +0800

    - Has a shorter way to create a list that get all the files, not the folders, and sort them. - Remove the loop that adds all the uppercase extensions

--- main.py
+++ main.py
@@ -1,51 +1,37 @@
 import os
 import shutil
+import glob
 
 #executable file is located in /dist/main 
 #path to directory - need to change for every new computer - after change need to make into an executable file again
-path = '/Users/hungtuan.nguyen/Downloads'
+path = '/Users/hungtuan.nguyen/Downloads/'
+            
+#get a list of all the files, sorted by modified time, descending
+files = list(filter(os.path.isfile, glob.glob(path+'*'))) #filter just files, not dir, and glob.glob is to get all files in the path
+files.sort(key=lambda x: -os.path.getmtime(x)) 
 
-#create a list of all the files 
-list_file = []
-
-#create a list of all the filename
-list_full = os.listdir(path)
-
-#find all the files in the Downloads dir and add to the list_file
-for a in list_full:
-    #only work with file that start without '.' to ignore hidden sys files
-    if not a.startswith('.'):
-        isFile = os.path.isfile(path+'/'+a)
-        if isFile:
-            list_file.append(a)
+#now list_full contains all files only, sorted by mtime
 
 #create a list of all image ext
-list_img_ext_lower = ['png','jpg','jpeg','webp', 'gif', 'avif']
-list_img_ext_full = []
-
-#add to the full list all the lower and upper case
-for i in range(len(list_img_ext_lower)):
-    list_img_ext_full.append(list_img_ext_lower[i])
-    list_img_ext_lower[i] = list_img_ext_lower[i].upper()
-    list_img_ext_full.append(list_img_ext_lower[i])
+list_img_ext = ['png','jpg','jpeg','webp', 'gif', 'avif']
 
 #check the extension of all the file in list_file
-for file in list_file:
+for file in files:
     name, ext = os.path.splitext(file)
     ext=ext[1:]
     #with images file, go with this flow
-    if ext in list_img_ext_full:
+    if ext.lower() in list_img_ext:
         #check if the "Image" folder exists:
-        if os.path.exists(path+'/'+'Images'):
-            shutil.move(path+'/'+file,path+'/'+'Images')
+        if os.path.exists(path+'Images'):
+            shutil.move(name+'.'+ext,path+'Images')
         else:
-            os.makedirs(path+'/'+'Images')
-            shutil.move(path+'/'+file,path+'/'+'Images')
+            os.makedirs(path+'Images')
+            shutil.move(name+'.'+ext,path+'Images')
     else:
         #with non-image file, go with this flow
-        if os.path.exists(path+'/'+ext):
-            shutil.move(path+'/'+file,path+'/'+ext)
+        if os.path.exists(path+ext):
+            shutil.move(name+'.'+ext,path+ext)
         else:
-            os.makedirs(path+'/'+ext)
-            shutil.move(path+'/'+file,path+'/'+ext)
+            os.makedirs(path+ext)
+            shutil.move(name+'.'+ext,path+ext)
 
