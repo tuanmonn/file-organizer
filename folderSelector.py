f8bcf417c292c4c0bc5bff62edaf16fca813cd58
Tuanmon <hungtuan.nguyen@shopee.com>
2023-02-16 16:58:33 +0800

    Major UX improvements 1. There's a window with instruction so users know what to do 2. Other: Split code into two files for better management

---
+++ folderSelector.py
@@ -0,0 +1,61 @@
+import os
+import shutil
+import glob
+import tkinter as tk
+from tkinter.filedialog import askdirectory
+
+#get the root dir of every laptop
+root_dir = os.path.expanduser('~')
+
+#get the path of the file we use to store user input
+input_file_dir = root_dir + '/' + 'user_input.txt'
+
+
+def run_program(): 
+    path = 0 
+    #open a window to confirm success
+    def success_conf():
+        window_success = tk.Toplevel(window)
+        window_success.title("Confirmation")
+        window_success_label = tk.Label(window_success,text="You're all set!\n Check your folder, everything is cleaned up now ✅")
+        window_success_label.pack()
+        window_success_button = tk.Button(window_success,text="Close", command=window_success.quit)
+        window_success_button.pack()
+        
+    #define a function to open folder selector
+    def folder_selector():
+        folder = askdirectory(title='Select Folder')
+        path = os.path.realpath(folder)
+        with open(input_file_dir,'w') as f:
+            f.write(path)
+            f.close()
+        success_conf()
+        return path
+    
+    #main program logic
+    if not os.path.exists(input_file_dir):
+        #start the window
+        window = tk.Tk()
+        window.title("File Organizer by Tuanmonn")
+        window.geometry("300x200")
+        
+        #create label
+        label_welcome = tk.Label(text="Welcome to File Organizer!")
+        label_ins = tk.Label(text="Please choose the folder you want to clean up")
+        label_welcome.pack()
+        label_ins.pack()
+        
+        #create a button to let user select folder
+        button = tk.Button(text="Select Folder",command=folder_selector)
+        button.pack()
+        
+        #keep the window open
+        window.mainloop()
+        return path
+        
+    else:
+        with open(input_file_dir,'r') as f:
+            path = f.read()
+            f.close()
+        return path
+                
--- main.py
+++ main.py
@@ -3,29 +3,13 @@ import shutil
 import glob
 from tkinter import Tk 
 from tkinter.filedialog import askdirectory
-
-#get the root dir of every laptop
-root_dir = os.path.expanduser('~')
-
-#get the path of the file we use to store user input
-input_file_dir = root_dir + '/' + 'user_input.txt'
-
-#check if that path exists (this program has been run before)
-#if not, prompt user to input the folder they want to do the cleanup
-if not os.path.exists(input_file_dir):
-    folder = askdirectory(title='Select Folder')
-    path = os.path.realpath(folder)
-    with open(input_file_dir,'w') as f:
-        f.write(path)
-        f.close()
-#if yes, check the file for the path that user has input
-else:
-    with open(input_file_dir,'r') as f:
-        path = f.read()
-        f.close()
+from folderSelector import *
+ 
+run_program()
+selected_path = run_program() 
             
 #get a list of all the files, sorted by modified time, descending
-files = list(filter(os.path.isfile, glob.glob(path+'/'+'*'))) #filter just files, not dir, and glob.glob is to get all files in the path
+files = list(filter(os.path.isfile, glob.glob(selected_path+'/'+'*'))) #filter just files, not dir, and glob.glob is to get all files in the path
 files.sort(key=lambda x: -os.path.getmtime(x)) 
 
 #now list_full contains all files only, sorted by mtime
@@ -40,16 +24,16 @@ for file in files:
     #with images file, go with this flow
     if ext.lower() in list_img_ext:
         #check if the "Image" folder exists:
-        if os.path.exists(path+'/'+'Images'):
-            shutil.move(name+'.'+ext,path+'/'+'Images')
+        if os.path.exists(selected_path+'/'+'Images'):
+            shutil.move(name+'.'+ext,selected_path+'/'+'Images')
         else:
-            os.makedirs(path+'/'+'Images')
-            shutil.move(name+'.'+ext,path+'/'+'Images')
+            os.makedirs(selected_path+'/'+'Images')
+            shutil.move(name+'.'+ext,selected_path+'/'+'Images')
     else:
         #with non-image file, go with this flow
-        if os.path.exists(path+'/'+ext):
-            shutil.move(name+'.'+ext,path+'/'+ext)
+        if os.path.exists(selected_path+'/'+ext):
+            shutil.move(name+'.'+ext,selected_path+'/'+ext)
         else:
-            os.makedirs(path+'/'+ext)
-            shutil.move(name+'.'+ext,path+'/'+ext)
+            os.makedirs(selected_path+'/'+ext)
+            shutil.move(name+'.'+ext,selected_path+'/'+ext)
 
